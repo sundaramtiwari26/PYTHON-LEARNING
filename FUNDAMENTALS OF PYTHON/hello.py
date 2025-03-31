81bf7cc41d0595a25b349e5bb9649877314aa684
sundaram tiwari <sundaramtiwari9795@gmail.com>
2025-03-31 06:14:39 +0530

    created repo

---
+++ FUNDAMENTALS OF PYTHON/hello.py
@@ -0,0 +1,19 @@
+print("hello world")
+a=18
+b=20
+print(a+b)
+#This is my first single line comment in python
+"""
+this is my first double line comment
+and i have also learn about variable 
+"""
+title=input(str("Enter your name....."))
+print(title)
+print(type(title))
+print(len(title))
+age=18
+print(type(age))
+pi=3.14
+print(type(pi))
+
+
