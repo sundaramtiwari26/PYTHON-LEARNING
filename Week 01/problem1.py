4120f26259df148348e19900a1a372442e03c9cf
sundaramtiwari26 <sundaramtiwari333@gmail.com>
2025-09-02 21:23:44 +0530

    first commit

---
+++ Week 01/problem1.py
@@ -0,0 +1,6 @@
+a=int(input("Enter first number...."))
+b=int(input("Enter second number...."))
+c=a
+a=b
+b=c
+print(f"The first number is {a} and second number is {b}")
---
+++ Week 01/problem4.py
@@ -0,0 +1,3 @@
+num=int(input("Enter a number....."))
+for i in range(1,10):
+    print(f"{num} X {i} = {num*i}")
